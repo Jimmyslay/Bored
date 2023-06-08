6d01288daa3198ebdb483e21a35d2188b4723849
MattsMadd <41555252+MattsMadd@users.noreply.github.com>
2023-06-07 21:47:24 -0700

    Update bored.py

--- bored.py
+++ bored.py
@@ -33,6 +33,12 @@ class MakeApiCall:
 if __name__ == "__main__":
   while True:
       chosen_category = input("which catagory do you want to filter? accessibility, activity, key, link, participants, price, or type? ")
+      if chosen_category == "type":
+        print("please choose education, recreational, social, diy, charity, cooking, relaxation, music, or busywork")
+      if chosen_category == "price":
+        print("choose a number between 0 and 0.5")
+      if chosen_category == "participants":
+        print("choose a number between 0 and 9")
       activity_type = input("Give the value to your chosen category (" + chosen_category + "): ")
       api_call = MakeApiCall("https://www.boredapi.com/api/activity?" + chosen_category + "=" + activity_type)
       done = input("Are you done (y or n)? ")
